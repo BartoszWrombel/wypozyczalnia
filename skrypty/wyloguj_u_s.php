435045399d158b33c2bcfd046416ebb332d8a4f5
Barti47 <barti0302.interia.pl@interia.pl>
2024-01-25 14:46:08 +0100

    lekkie zmiany kosmetyczne w wyloguj

--- skrypty/wyloguj_u_s.php
+++ skrypty/wyloguj_u_s.php
@@ -1,13 +1,11 @@
 <?php
-	require '../baza.php';
-	require 'loguj_u_s.php';
-		session_start();
-		//sprawdzenie czy wpisany login istnieje w bazie danych
-		if (isset($_SESSION['sesja'])) {
-			unset($_SESSION['sesja']);
-			echo "Jesteś wylogowany: </b>";
-		}
-		else {
-			echo "Nie jesteś zalogowany";
-		}
-?>
+require '../baza.php';
+require 'loguj_u_s.php';
+session_start();
+//sprawdzenie czy sesja istnieje
+if (isset($_SESSION['sesja'])) {
+	unset($_SESSION['sesja']);
+	echo "Jesteś wylogowany: </b>";
+} else {
+	echo "Nie jesteś zalogowany";
+}
