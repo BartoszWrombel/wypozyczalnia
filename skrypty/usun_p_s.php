71b4b5d4fc293e2a883cbb8d8a1722dffe3d5cf1
Barti47 <barti0302.interia.pl@interia.pl>
2024-01-25 21:07:23 +0100

    usuwanie rezerwacji

--- skrypty/usun_p_s.php
+++ skrypty/usun_p_s.php
@@ -1,6 +1,41 @@
 <?php
-	require '../baza.php';
-	//sprawdzenie czy pacjent o podanym numerze pesel istnieje
+require '../baza.php';
+
+
+if (isset($_POST['usun_rezerwacje'])) {
+	$id_wypozyczenia = $_POST['id_wypozyczenia'];
+
+	// Sprawdzenie, czy rezerwacja o podanym identyfikatorze istnieje
+	$sql_rezerwacje = 'SELECT COUNT(*) 
+					   FROM wypozyczenia 
+					   WHERE id_wypozyczenia = :id_wypozyczenia';
+	$stmt_rezerwacje = $pdo->prepare($sql_rezerwacje);
+	$stmt_rezerwacje->bindValue(':id_wypozyczenia', $id_wypozyczenia, PDO::PARAM_INT);
+	$stmt_rezerwacje->execute();
+	$ilosc_rezerwacji = $stmt_rezerwacje->fetchColumn();
+
+	if ($ilosc_rezerwacji > 0) {
+		// Usunięcie rezerwacji
+		$sql_usun_rezerwacje = 'DELETE FROM wypozyczenia 
+								WHERE id_wypozyczenia = :id_wypozyczenia';
+		$stmt_usun_rezerwacje = $pdo->prepare($sql_usun_rezerwacje);
+		$stmt_usun_rezerwacje->bindValue(':id_wypozyczenia', $id_wypozyczenia, PDO::PARAM_INT);
+
+		if ($stmt_usun_rezerwacje->execute()) {
+			echo 'Rezerwacja o identyfikatorze ' . $id_wypozyczenia . ' została usunięta.';
+			// header("Location: {$_SERVER['PHP_SELF']}");
+		} else {
+			echo '<h4 style="color:red;">Błąd podczas usuwania rezerwacji!</h4>';
+		}
+	} else {
+		echo 'Rezerwacja o podanym identyfikatorze nie istnieje.';
+	}
+}
+?>
+
+<!-- <?php
+		require '../baza.php';
+		//sprawdzenie czy pacjent o podanym numerze pesel istnieje
 		$sql1 = 'SELECT Count(numer_pesel)
 				FROM pacjent
 				WHERE numer_pesel = :numer_pesel';
@@ -8,24 +43,22 @@
 		$stmt1->bindValue(':numer_pesel', $_POST['numer_pesel'], PDO::PARAM_STR);
 		$stmt1->execute();
 		$ilosc_pesel = $stmt1->fetchColumn();
-		
-		if ($ilosc_pesel !== 0) {	
+
+		if ($ilosc_pesel !== 0) {
 			$sql2 = 'DELETE FROM pacjent
 					WHERE numer_pesel = :numer_pesel';
-			$stmt2 = $pdo -> prepare($sql2);
+			$stmt2 = $pdo->prepare($sql2);
 			$stmt2->bindValue(':numer_pesel', $_POST['numer_pesel'], PDO::PARAM_INT);
 			$stmt2->execute();
 			$pesel_del = $_POST['numer_pesel'];
-	
+
 			if ($_POST['numer_pesel'] != null) {
-				
-				echo 'Usunięto pacjenta: o NUMERZE PESEL: '.$pesel_del;
-			}
-			else {
+
+				echo 'Usunięto pacjenta: o NUMERZE PESEL: ' . $pesel_del;
+			} else {
 				echo '<h4 style="color:red;">Nie wybrano pacjenta!</h4>';
 			}
-		}
-		else {
+		} else {
 			echo 'Podany pacjent w bazie nie istnieje.';
 		}
-?>
+		?> -->
--- widoki/usun_p.php
+++ widoki/usun_p.php
@@ -1,70 +1,95 @@
 <!DOCTYPE html>
 <html lang="pl">
-<head>	
-<meta charset="utf-8">
-<title>Szpital powiatowy w Zakolu Dolnym</title>
-<link rel="stylesheet" type="text/css" href="../style/style.css" />
+
+<head>
+	<meta charset="utf-8">
+	<title>Wypożyczalnia samochodów</title>
+	<link rel="stylesheet" type="text/css" href="../style/style.css" />
 </head>
+
 <body>
 	<div class="wrapper">
-	<a href="rejestruj.php" class="right">Rejestracja</a>
-	<a href="wyloguj.php" class="right">Wyloguj</a>
-	<a href="login.php" class="right">Zaloguj</a>
+		<a href="rejestruj.php" class="right">Rejestracja</a>
+		<a href="wyloguj.php" class="right">Wyloguj</a>
+		<a href="login.php" class="right">Zaloguj</a>
 
-	<h1>SZPITAL POWIATOWY W ZAKOLU DOLNYM</h1>
-	<nav>
-		<ul>
-			<li><a href="../index.php">Home</a></li>
-			<li><a href="lista_p.php">Lista pacjentów</a></li>
-			<li><a href="wyszukaj_p.php">Wyszukaj pacjenta</a></li>
-			<li><a href="dodaj_p.php">Dodaj pacjenta</a></li>
-			<li><a href="edytuj_p.php">Edytuj dane</a></li>
-			<li><a href="usun_p.php">Wyrejestruj pacjenta</a></li>
-		</ul>
-		</ul>
-	</nav>	
-	<h2>Wyrejestruj pacjenta</h2>
-	<?php
+		<h1>Wypożyczalnia samochodów</h1>
+		<nav>
+			<ul>
+				<li><a href="../index.php">Home</a></li>
+				<li><a href="lista_p.php">Lista pacjentów</a></li>
+				<li><a href="wyszukaj_p.php">Wyszukaj pacjenta</a></li>
+				<li><a href="dodaj_p.php">Dodaj pacjenta</a></li>
+				<li><a href="edytuj_p.php">Edytuj dane</a></li>
+				<li><a href="usun_p.php">Wyrejestruj pacjenta</a></li>
+			</ul>
+			</ul>
+		</nav>
+		<h2>Usuwanie rezerwacji</h2>
+		<?php
 		session_start();
-		if(isset($_SESSION['sesja'])) {
-			if ($_SESSION['uprawnienia'] == 'pielęgniarka') {
-			?>
+		if (isset($_SESSION['sesja'])) {
+			// if ($_SESSION['uprawnienia'] == 'pielęgniarka') {
+		?>
 			<form name="dane" action="usun_p.php" method="post">
-			
-			
-			<input type="number" name="numer_pesel" placeholder="podaj numer pesel" style="width:20%;display:inline;text-align:center;" />
-			<input type="submit" name="submit" value="Wyrejestruj" style="width:20%;display:inline;" />
+				<input type="number" name="id_wypozyczenia" placeholder="podaj id wypozyczenia" style="width:20%;display:inline;text-align:center;" />
+				<input type="submit" name="usun_rezerwacje" value="usun rezerwacje" style="width:20%;display:inline;" />
 			</form>
 			<table>
-			<tr>
-				<th>Imię pacjenta</th><th>Nazwisko pacjenta</th><th>Numer PESEL</th>
-				<th>Imię lekarza</th><th>Nazwisko lekarza</th><th>Oddział</th><th>Historia choroby</th>
-			</tr>
-			<?php
-				require ('../skrypty/lista_p_s.php');
-				foreach ($pacjenci as $pacjent => $link) {
-			?>
-			<tr>
-				<td><?= $link['imie'] ?></td><td><?= $link['nazwisko'] ?></td><td><?= $link['numer_pesel'] ?></td>
-				<td><?= $link['imie_l'] ?></td><td><?= $link['nazwisko_l'] ?></td>
-				<td><?= $link['oddzial'] ?></td><td><?= $link['historia_choroby'] ?></td>
-			</tr>
-			<?php
-			}
-			?>
+				<tr>
+					<th>ID wypożyczenia</th>
+					<th>Imię klienta</th>
+					<th>Nazwisko klienta</th>
+					<th>Numer PESEL klienta</th>
+					<th>Marka samochodu</th>
+					<th>Model samochodu</th>
+					<th>Wariant samochodu</th>
+					<th>Kolor samochodu</th>
+					<th>Numer rejestracyjny samochodu</th>
+					<th>Rok produkcji samochodu</th>
+					<th>Cena za dzień</th>
+					<th>Status samochodu</th>
+					<th>Data wypożyczenia</th>
+					<th>Data zwrotu</th>
+					<th>Koszt całkowity</th>
+					<th>Uwagi</th>
+				</tr>
+				<?php
+				require('../skrypty/lista_p_s.php');
+				foreach ($wypozyczenia as $wypozyczenia1 => $link) {
+				?>
+					<tr>
+						<td><?= $link['id_wypozyczenia'] ?></td>
+						<td><?= $link['imie'] ?></td>
+						<td><?= $link['nazwisko'] ?></td>
+						<td><?= $link['pesel'] ?></td>
+						<td><?= $link['marka'] ?></td>
+						<td><?= $link['model'] ?></td>
+						<td><?= $link['wariant'] ?></td>
+						<td><?= $link['kolor'] ?></td>
+						<td><?= $link['numer_rejestracyjny'] ?></td>
+						<td><?= $link['rok_produkcji'] ?></td>
+						<td><?= $link['cena_za_dzien'] ?></td>
+						<td><?= $link['status_samochodu'] ?></td>
+						<td><?= $link['data_wypozyczenia'] ?></td>
+						<td><?= $link['data_zwrotu'] ?></td>
+						<td><?= $link['koszt_calkowity'] ?></td>
+						<td><?= $link['uwagi'] ?></td>
+					</tr>
+				<?php
+				}
+				?>
 			</table>
-			<?php
-				require ('../skrypty/usun_p_s.php');
-			}
-			else {
-				echo "Nie masz wystarczających uprawnień. Proszę się zalogować z uprawieniami pielęgniarki";
-			}
+		<?php
+			require('../skrypty/usun_p_s.php');
+		} else {
+			echo "Nie masz wystarczających uprawnień. Proszę się zalogować z uprawieniami pielęgniarki";
 		}
-		else {
-			echo 'Proszę się zalogować z uprawieniami pielęgniarki.';
-		}
-			?>
-   	</div>
-	</body>
+		// } else {
+		// 	echo 'Proszę się zalogować z uprawieniami pielęgniarki.';
+		// }
+		?>
+	</div>
+</body>
+
 </html>
-	
